ca91f6093d4c6fd65dcd50bb84beb26f90677618
Ilya Petrov <ilya.muromec@gmail.com>
2014-12-21 15:10:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setup(
     name = 'gost89',
     version = '0.0.1',
     description = 'Gost89 bindings for python',
+    url='https://github.com/dstucrypt/python-gost89',
+    author='Ilya Petrov',
+    author_email='ilya.muromec@gmail.com',
     ext_modules = [module1],
     packages=[
         'gost89',
